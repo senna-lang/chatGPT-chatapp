36f1d64a220cd4e97b2e766643db03bc84eba6da
senna-lang <asrsena3302@gmail.com>
2023-10-24 19:59:16 +0900

    [feat] userがログアウト状態だとログインページにリダイレクとされる

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
-import Sidebar from "./components/Sidebar";
-import Chat from "./components/Chat";
+'use client'
+import Sidebar from './components/Sidebar';
+import Chat from './components/Chat';
+import { useAppContext } from '@/context/AppContext';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+  const { user } = useAppContext();
+  const router = useRouter();
+
+  if (!user) {
+    router.push('/auth/login')
+  }
   return (
     <div className=" h-screen flex flex-col items-center justify-center">
       <div className=" h-full flex w-[1280px]">
