99580f5314bf9ac05744b2db7a069ec566782ba1
senna-lang <asrsena3302@gmail.com>
2023-10-24 20:56:37 +0900

    [feat] ログアウト時のリダイレクトを削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,8 @@
-'use client'
+'use client';
 import Sidebar from './components/Sidebar';
 import Chat from './components/Chat';
-import { useAppContext } from '@/context/AppContext';
-import { useRouter } from 'next/navigation';
 
 export default function Home() {
-  const { user } = useAppContext();
-  const router = useRouter();
-
-  if (!user) {
-    router.push('/auth/login')
-  }
   return (
     <div className=" h-screen flex flex-col items-center justify-center">
       <div className=" h-full flex w-[1280px]">
