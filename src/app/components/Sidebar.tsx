1ca95a5af829ce1e3b26a3e1051ed936d1ac586e
senna-lang <asrsena3302@gmail.com>
2023-10-24 19:37:31 +0900

    [feat]新規ルーム追加機能実装

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -6,7 +6,9 @@ import {
   onSnapshot,
   orderBy,
   query,
+  serverTimestamp,
   where,
+  addDoc
 } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react';
 import { RiLogoutBoxRLine } from 'react-icons/Ri';
@@ -20,10 +22,10 @@ type Room = {
 };
 
 const Sidebar = () => {
-  const { user, userId, setSelectedRoom} = useAppContext();
+  const { user, userId, setSelectedRoom } = useAppContext();
   const [rooms, setRooms] = useState<Room[]>([]);
 
-  useEffect(() => {
+  useEffect(() => {//ユーザーIDに応じてFirestoreからroomを取得してくる関数
     if (user) {
       const fetchRooms = async () => {
         const roomCollectionRef = collection(db, 'rooms');
@@ -32,7 +34,7 @@ const Sidebar = () => {
           where('userid', '==', userId),
           orderBy('createdAt')
         );
-        const unsubscribe = onSnapshot(q, snapshot => {
+        const unsubscribe = onSnapshot(q, snapshot => {//db内のデータの変更を監視
           const newRooms: Room[] = snapshot.docs.map(doc => ({
             id: doc.id,
             name: doc.data().name,
@@ -49,14 +51,31 @@ const Sidebar = () => {
     }
   }, [userId]);
 
-  const selectRoom = (roomId:string) => {
-    setSelectedRoom(roomId);
+  const addNewRoom = async() => { //新しいルームを作成する関数
+   const roomName =  prompt('ルーム名を入力してください');
+   if(roomName) {
+    const newRoomRef = collection(db,'rooms');
+    await addDoc (newRoomRef, {
+      name: roomName,
+      userid: userId,
+      createdAt: serverTimestamp(),
+    })
+   }
   }
-  
+
+  // console.log (rooms)
+
+  const selectRoom = (roomId: string) => {//ルームの切り替えを行う関数
+    setSelectedRoom(roomId);
+  };
+
   return (
     <div className="bg-custom-blue h-full overflow-y-auto px-5 flex flex-col">
       <div className=" flex-grow ">
-        <div className="flex justify-evenly items-center border mt-2 rounded-md cursor-pointer hover:bg-blue-800 duration-200">
+        <div
+          className="flex justify-evenly items-center border mt-2 rounded-md cursor-pointer hover:bg-blue-800 duration-200"
+          onClick={addNewRoom}
+        >
           <span className="text-white p-4 text-2xl">+</span>
           <h1 className=" text-white text-xl font-semibold p-4">New Chat</h1>
         </div>
